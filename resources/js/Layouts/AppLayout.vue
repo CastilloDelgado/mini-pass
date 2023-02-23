1d7d38ec3ca51916c6b98300ffbb4ca941c0a4c9
Marco Castillo <marcocastillo1997@gmail.com>
2023-02-22 22:58:16 -0600

    Adding logo to navbar

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -237,7 +237,10 @@ const logout = () => {
                                                 type="button"
                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150"
                                             >
-                                                {{ $page.props.auth.user.name }}
+                                                ¡Hola,
+                                                {{
+                                                    $page.props.auth.user.name
+                                                }}!
 
                                                 <svg
                                                     class="ml-2 -mr-0.5 h-4 w-4"
