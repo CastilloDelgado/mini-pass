21c4e698a56d43f8c41e16c8d649ccb2236c0b18
Marco Castillo <marcocastillo1997@gmail.com>
2023-04-04 03:03:51 -0600

    Changing stripe secrets to env file

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -8,80 +8,9 @@ use Exception;
 
 class StripeController extends Controller
 {
-    public function getSessionTest(Sale $sale)
-    {
-        $stripe = new \Stripe\StripeClient(env('STRIPE_API_KEY'));
-
-        // // Adding tickets summary to sale model
-        // $sale->tickets;
-        // $tickets = $sale->tickets;
-        // $ticketsSummary = [];
-
-        // foreach ($tickets as $ticket) {
-        //     if (isset($ticketsSummary[$ticket->ticket_type_id])) {
-        //         $ticketsSummary[$ticket->ticket_type_id] += 1;
-        //     } else {
-        //         $ticketsSummary[$ticket->ticket_type_id] = 1;
-        //     }
-        // }
-
-        // $saleSummary = [];
-        // $total = 0;
-
-        // foreach ($ticketsSummary as $type => $quantity) {
-        //     $ticketType = TicketType::find($type);
-        //     $total += ($ticketType->price + ($ticketType->price * 0.2)) * $quantity;
-        //     $saleSummary[] = [
-        //         "title" => $ticketType->title,
-        //         "description" => $ticketType->description,
-        //         "price" => $ticketType->price,
-        //         "charge" => $ticketType->price * 0.2,
-        //         "quantity" => $quantity,
-        //         "total" => ($ticketType->price + ($ticketType->price * 0.2)) * $quantity,
-        //     ];
-        // }
-
-        // $lineItems = [];
-        // foreach ($saleSummary as $item) {
-        //     $lineItems[] = [
-        //         'price_data' => [
-        //             'currency' => 'mxn',
-        //             'unit_amount' => $item->price * 100,
-        //             'product_data' => [
-        //                 'name' => "Test"
-        //             ],
-        //         ],
-        //         'quantity' => $item->quantity
-        //     ];
-        // }
-
-        $checkout = $stripe->checkout->sessions->create([
-            'success_url' => 'https://localhost:8000/success',
-            'line_items' => [
-                [
-                    'price_data' => [
-                        'currency' => 'mxn',
-                        'unit_amount' => 50000,
-                        'product_data' => [
-                            'name' => "Boletos para tame impala"
-                        ],
-                    ],
-                    'quantity' => 1
-                ],
-            ],
-            'mode' => 'payment',
-        ]);
-
-        return $checkout;
-    }
-
     public function getSession(Sale $sale)
     {
         try {
-
-
-            $stripe = new \Stripe\StripeClient(env('STRIPE_API_KEY'));
-
             // Adding tickets summary to sale model
             $sale->tickets;
             $tickets = $sale->tickets;
@@ -130,6 +59,8 @@ class StripeController extends Controller
                     'quantity' => $quantity
                 ];
             }
+
+            $stripe = new \Stripe\StripeClient(config('app.stripe_key'));
 
             $checkout = $stripe->checkout->sessions->create([
                 'success_url' => route('stripe.checkout.success'),
